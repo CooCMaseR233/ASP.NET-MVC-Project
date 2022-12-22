0fc2c308f0cfbe76e405f7f0114470a36bb98f33
User <User@DESKTOP-P96V8L6>
2022-12-22 14:43:21 +0200

    Add project files.

---
+++ ASP.NET-MVC Project/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using ASP.NET_MVC_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text.Encodings.Web;
+
+namespace ASP.NET_MVC_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
---
+++ ASP.NET-MVC Project/Models/Movie.cs
@@ -0,0 +1,6 @@
+﻿namespace ASP.NET_MVC_Project.Models
+{
+    public class Movie
+    {
+    }
+}
